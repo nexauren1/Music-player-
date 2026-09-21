08ece5428e80164298c325f446b0deadc4f2c1b0
nexauren1 <nexaurenstore@gmail.com>
2026-09-21 11:20:50 +0200

    fix: add menu activity and ringtone imports

--- app/src/main/java/com/nexauren/musicplayer/MainActivity.java
+++ app/src/main/java/com/nexauren/musicplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.nexauren.musicplayer;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.ContentUris;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
