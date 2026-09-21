5cdda7f89099b0717b1639a83eb00c1d632ba154
nexauren1 <nexaurenstore@gmail.com>
2026-09-21 19:31:13 +0200

    fix: add metadata retriever import

--- app/src/main/java/com/nexauren/musicplayer/EditTagsActivity.java
+++ app/src/main/java/com/nexauren/musicplayer/EditTagsActivity.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
