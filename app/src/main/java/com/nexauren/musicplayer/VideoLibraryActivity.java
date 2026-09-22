852967693502dcb33634289388b951750be7e9ef
nexauren1 <nexaurenstore@gmail.com>
2026-09-22 18:53:05 +0200

    fix: import ViewGroup for video library layout

--- app/src/main/java/com/nexauren/musicplayer/VideoLibraryActivity.java
+++ app/src/main/java/com/nexauren/musicplayer/VideoLibraryActivity.java
@@ -12,6 +12,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
